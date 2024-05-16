7ac8a8df778c960c7be7a34c1a91bda943f60934
ValerioL96 <v.lembo1396@gmail.com>
2024-05-16 16:35:56 +0200

    Added function in javascript

--- Palindroma/js/script.js
+++ Palindroma/js/script.js
@@ -5,3 +5,31 @@
 
 const userWord = prompt('type a word');
 
+//Creare una funzione per capire se la parola inserita è palindroma:
+
+//2) Creo una funzione che mi permetta di capire se la parola digitata dall'utente sia uguale alla stessa parola inversa
+
+function reverseUserWord(word){
+    const palindroma = word;
+    let reversePalindroma = '';
+  
+
+//questo grazie all'uso di un ciclo for 
+
+    for(let i= palindroma.length-1; i>0; i--){
+      reversePalindroma+= palindroma[i];
+    }
+    return(reversePalindroma);
+  
+  }
+  
+  let reverseWord = reverseUserWord(userWord);
+       
+
+// e una condizione.
+
+  if(userWord == reverseWord){
+    console.log(' It is a palindromic word');
+  }else{
+    console.log('It is not a palindromic word ');
+  }
